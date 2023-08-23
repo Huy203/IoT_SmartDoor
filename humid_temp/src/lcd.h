c1499556a39a671daefd01868170a942a4c3f7bc
Ngan Truc <ngantruc2003@gmail.com>
2023-08-23 22:53:08 +0700

    merge final

---
+++ humid_temp/src/lcd.h
@@ -0,0 +1,69 @@
+#include "humidTemp.h"
+
+static LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_LINES);
+
+int postID = 0;
+
+byte degreeSymbol[8] = {
+    B00110,
+    B01001,
+    B00110,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000};
+
+void LCDalert()
+{
+    lcd.clear();
+    lcd.setCursor(3, 0);
+    lcd.print("DANGEROUS!");
+}
+
+void LCDreset()
+{
+    postID = 0;
+}
+
+int setLCD(float temp, float humid, TypeTemp type)
+{
+    if (type == TypeTemp::Fahrenheit && temp > 150)
+    {
+        postID++;
+    }
+    if (type == TypeTemp::Cescius && temp > 60)
+    {
+        postID++;
+    }
+    if (postID == 0)
+    {
+        ostringstream stream;
+        stream << fixed << setprecision(2) << temp;
+
+        string strTemp;
+        strTemp = "Temp: " + stream.str();
+
+        lcd.clear();
+        const char *buffer = strTemp.c_str();
+        lcd.createChar(1, degreeSymbol);
+        lcd.setCursor(0, 0);
+        lcd.print(buffer);
+        lcd.write(1);
+        if (type == TypeTemp::Fahrenheit)
+            lcd.print("F");
+        else
+            lcd.print("C");
+
+        stream.str("");
+        stream << fixed << setprecision(2) << humid;
+        string strHumid = "Humid: " + stream.str() + "%";
+        buffer = strHumid.c_str();
+        lcd.setCursor(0, 1);
+        lcd.print(buffer);
+    }
+    else
+        LCDalert();
+
+    return postID;
+}
--- src/define.h
+++
@@ -1,8 +0,0 @@
-enum TypeTemp
-{
-    Cescius = false,
-    Fahrenheit = true,
-};
-
-#define FIREBASE_HOST "https://testfirebase-165c4-default-rtdb.asia-southeast1.firebasedatabase.app/"
-#define FIREBASE_AUTH "AIzaSyC3HBYPCoFbrjZUk7O-XpU6YNw_vVnC_Xc"
